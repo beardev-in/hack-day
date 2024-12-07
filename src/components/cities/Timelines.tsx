dc3888cf4a24cd61734a71b5b444e30d27b1dec1
SatyamKumar <kumarsatyam04.2000@gmail.com>
2024-12-07 23:58:16 +0530

    handled the logic for showing timeline for selected country

--- src/components/cities/Timelines.tsx
+++ src/components/cities/Timelines.tsx
@@ -2,7 +2,7 @@
 
 import { RootState } from "@/app/redux/store";
 import React from "react";
-import { useSelector, useDispatch } from "react-redux"; // Import useDispatch
+import { useSelector, useDispatch } from "react-redux";  // Import useDispatch
 import { setSelectedCity } from "@/app/redux/features/EventSlice"; // Import the action
 import countries from "@/countriesData";
 import Link from "next/link";
@@ -37,90 +37,93 @@ const Timelines: React.FC<TimelinesProps> = ({ position }) => {
   // Function to handle city card click and dispatch the selected city to the store
   const handleCityClick = (cityName: string) => {
     dispatch(setSelectedCity(cityName)); // Dispatch the selected city name
+    
   };
 
   return (
     <div className="relative select-none">
       {countryData.cities.map((city, index) => (
-        <Link
+        <div
           key={index}
-          href={`/hackathon/${city.name.toLowerCase()}`} // Dynamically generate the URL
-          onClick={() => handleCityClick(city.name)} // Dispatch action on click
+          className={`w-[332px] sm:w-[350px] lg:w-[380px] h-[86px] my-2 p-4 rounded-md ${
+            highlightTimeline(index)
+              ? "bg-custom-gradient border border-gray-800"
+              : ""
+          }`}
+          onClick={() => handleCityClick(city.name)} // Add onClick handler
         >
-          <div
-            className={`w-[332px] sm:w-[350px] lg:w-[380px] h-[86px] my-2 p-4 rounded-md ${
-              highlightTimeline(index)
-                ? "bg-custom-gradient border border-gray-800"
-                : ""
-            }`}
-          >
-            <div className="flex items-center gap-2">
-              {/* City Name */}
-              <h2
-                className={`${
-                  highlightTimeline(index)
-                    ? "opacity-100 text-xl text-[18px]"
-                    : "opacity-50 text-[11px]"
-                }`}
-              >
-                {city.name}
-              </h2>
-              {/* Icon for highlighting the event */}
-              {highlightTimeline(index) ? (
-                <span>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="14"
-                    height="14"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="#FFFFFF"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="lucide lucide-square-arrow-out-up-right"
-                  >
-                    <path d="M21 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1-2-2h6" />
-                    <path d="m21 3-9 9" />
-                    <path d="M15 3h6v6" />
-                  </svg>
-                </span>
-              ) : (
-                <span>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="8"
-                    height="8"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="#969696"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="lucide lucide-square-arrow-out-up-right"
-                  >
-                    <path d="M21 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1-2-2h6" />
-                    <path d="m21 3-9 9" />
-                    <path d="M15 3h6v6" />
-                  </svg>
-                </span>
-              )}
-            </div>
-
-            {/* Event Description */}
-            <div className="flex flex-col items-start gap-">
-              <p
-                className={`${
-                  highlightTimeline(index)
-                    ? "opacity-100 text-[18px] text-[#969696]"
-                    : "opacity-50 text-[10px]"
-                }`}
-              >
-                <strong>Event Name:</strong> {city.event.name}
-              </p>
-            </div>
+          <div className="flex items-center gap-2">
+            {/* City Name */}
+            <h2
+              className={`${
+                highlightTimeline(index)
+                  ? "opacity-100 text-xl text-[18px]"
+                  : "opacity-50 text-[11px]"
+              }`}
+            >
+              {city.name}
+            </h2>
+            {/* Icon for highlighting the event */}
+            {highlightTimeline(index) ? (
+              <Link
+              key={index}
+              href={`/hackathon/${city.name.toLowerCase()}`} // Dynamically generate the URL
+              onClick={() => handleCityClick(city.name)} // Dispatch action on click
+            >
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="14"
+                  height="14"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="#FFFFFF"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="lucide lucide-square-arrow-out-up-right"
+                >
+                  <path d="M21 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h6" />
+                  <path d="m21 3-9 9" />
+                  <path d="M15 3h6v6" />
+                </svg>
+              </span>
+              </Link>
+            ) : (
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="8"
+                  height="8"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="#969696"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="lucide lucide-square-arrow-out-up-right"
+                >
+                  <path d="M21 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h6" />
+                  <path d="m21 3-9 9" />
+                  <path d="M15 3h6v6" />
+                </svg>
+              </span>
+            )}
           </div>
-        </Link>
+
+          {/* Event Description */}
+          <div className="flex flex-col items-start gap-">
+            <p
+              className={`${
+                highlightTimeline(index)
+                  ? "opacity-100 text-[18px] text-[#969696]"
+                  : "opacity-50 text-[10px]"
+              }`}
+            >
+              <strong>Event Name:</strong> {city.event.name}
+            </p>
+          </div>
+        </div>
       ))}
     </div>
   );
